3f4ffeaeaa6d28d965268d11be6db2e0a1f8e142
Mannat Gupta <mannatgupta@Mannats-MacBook-Air.local>
2023-12-05 23:21:06 +0530

    multi search functionality - done

--- mysearchfeature/src/App.js
+++ mysearchfeature/src/App.js
@@ -1,18 +1,28 @@
 import {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Users } from './Users';
+import Table from './Table';
 
 function App() {
   const [query, setQuery] = useState(null);
+
+  const keys = ['first_name', 'last_name', 'email'];
+
+  const search = (data) => {
+    return data.filter(item => keys.some(key => item[key].toLowerCase().includes(query)));
+  };
+
   return (
     <div className="App">
       <input type='text'
       className='searchBar'
+      placeholder='Serach here'
       onChange={(e) => setQuery(e.target.value)}/>
-      {Users.filter(user => user.first_name.toLowerCase().includes(query)).map(user => {
+
+      {/* {Users.filter(user => user.first_name.toLowerCase().includes(query)).map(user => {
        return <div key={user.id}>{user.first_name}</div>
-      })}
+      })} */}
+      <Table data={search(Users)}/>
     </div>
   );
 }
